e66deeb2c6dccce9fe3dfd727db99a32297384a0
usa4060 <cocoa9805@naver.com>
2022-01-19 17:55:27 +0900

    ReadLEDBillboard

---
+++ LEDBillboard/LEDBillboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDBillboard
+//
+//  Created by admin on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
