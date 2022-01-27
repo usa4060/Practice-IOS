73a2bb3724dd520191fa1d952ec1942f1a93b91b
usa4060 <cocoa9805@naver.com>
2022-01-27 15:44:45 +0900

    DailyDiary

---
+++ DailyDiary/DailyDiary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  DailyDiary
+//
+//  Created by admin on 2022/01/27.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ DailyDiary/DailyDiary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  DailyDiary
+//
+//  Created by admin on 2022/01/27.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
