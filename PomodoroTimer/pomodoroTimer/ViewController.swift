0ddc0b6ef1dd4a49feef9fc3d27e09e22e15d24b
usa4060 <cocoa9805@naver.com>
2022-02-05 23:36:41 +0900

    PomodoroTimer

--- PomodoroTimer/pomodoroTimer/ViewController.swift
+++ PomodoroTimer/pomodoroTimer/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  pomodoroTimer
-//
-//  Created by admin on 2022/02/04.
-//
+
 
 import UIKit
 
@@ -11,7 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
